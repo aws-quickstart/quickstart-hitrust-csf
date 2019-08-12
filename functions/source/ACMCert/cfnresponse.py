35e9be5f250277e5d0ff15396aea7a04d978988a
Friedman <ajfriedm@186590cffb83.ant.amazon.com>
2019-08-12 03:35:21 -0700

    debugging

--- functions/source/ACMCert/cfnresponse.py
+++ functions/source/ACMCert/cfnresponse.py
@@ -30,7 +30,7 @@ def send(event, context, responseStatus, responseData, physicalResourceId, reaso
 
     json_responseBody = json.dumps(responseBody)
    
-    print "Response body:\n" + json_responseBody
+    print ("Response body:\n" + json_responseBody)
 
     headers = {
         'content-type' : '', 
@@ -41,6 +41,6 @@ def send(event, context, responseStatus, responseData, physicalResourceId, reaso
         response = requests.put(responseUrl,
                                 data=json_responseBody,
                                 headers=headers)
-        print "Status code: " + response.reason
+        print ("Status code: " + response.reason)
     except Exception as e:
-        print "send(..) failed executing requests.put(..): " + str(e)
+        print ("send(..) failed executing requests.put(..): " + str(e))
